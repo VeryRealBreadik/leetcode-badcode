cebe5ef8e7c2647a4111e7aee0d69a8e33c360f2
VeryRealBreadik <xjie6yiiiek228822@gmail.com>
2025-06-09 17:16:42 +0300

    1 easy challenge: TwoSum

---
+++ 1_two_sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {}
+        for i in range(len(nums)):
+            remainder = target - nums[i]
+            if remainder in nums_dict:
+                return nums_dict[remainder], i
+            nums_dict[nums[i]] = i
+
+def main():
+    solution = Solution()
+    result = solution.twoSum([3, 3], 6)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
