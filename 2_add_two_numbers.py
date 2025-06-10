37fc90dc6a6bf6dd503abd0f7c9d6822db458b6a
VeryRealBreadik <xjie6yiiiek228822@gmail.com>
2025-06-10 19:05:46 +0300

    2 medium challenges: 2;3

---
+++ 2_add_two_numbers.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy_head = ListNode()
+        current_node = dummy_head
+        carry = 0
+        while l1 or l2 or carry != 0:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            sum = l1_val + l2_val + carry
+            carry = sum // 10
+            new_node = ListNode(sum % 10)
+            current_node.next = new_node
+            current_node = current_node.next
+            
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy_head.next
+
+def main():
+    solution = Solution()
+    
+    l1 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9)))))))
+    l2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
+    result = solution.addTwoNumbers(l1, l2)
+    while result:
+        print(result.val, end=", ")
+        result = result.next
+
+
+if __name__ == "__main__":
+    main()
---
+++ 3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        chars_dict = {}
+        left_pointer = 0
+        max_len = 0
+        
+        for right_pointer, char in enumerate(s):
+            if char in chars_dict and chars_dict[char] >= left_pointer:
+                left_pointer = chars_dict[char] + 1
+            
+            max_len = max(max_len, right_pointer - left_pointer + 1)
+            chars_dict[char] = right_pointer
+        
+        return max_len
+
+def main():
+    solution = Solution()
+    string1 = "abba"
+    string2 = "bbbbb"
+    string3 = "pwwkew"
+    result1 = solution.lengthOfLongestSubstring(string1)
+    result2 = solution.lengthOfLongestSubstring(string2)
+    result3 = solution.lengthOfLongestSubstring(string3)
+    print(f"{string1}: {result1}")
+    print(f"{string2}: {result2}")
+    print(f"{string3}: {result3}")
+
+
+if __name__ == "__main__":
+    main()
